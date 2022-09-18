65d73b3f5030526b9f1863b7732aff8f287eff9b
Federico Levin <federico.levin@despegar.com>
2022-09-18 12:01:30 -0300

    file system

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,12 +1,24 @@
+const fs = require('fs');
+
 const tablaDeMultiplicacion = (base) => {
     console.clear();
     console.log('====================');
     console.log(`Tabla del ${base}`);
     console.log('====================');
+
+    let salida = '';
     
     for (let i = 1; i <= 10; i++) {
-        console.log(`${base} x ${i} = ${base*i}`);
+        // console.log(`${base} x ${i} = ${base*i}`);
+        salida += (`${base} x ${i} = ${base*i}\n`);
     }
+    
+    fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+        if (err) throw err;
+        console.log(`Table del ${base} creada.`);
+    });
+
+    console.log(salida);
 };
 
-tablaDeMultiplicacion(5);
+tablaDeMultiplicacion(10);
