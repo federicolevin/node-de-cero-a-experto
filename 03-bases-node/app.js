334fe4df00791970fc64e8392628127b99c2c11e
Federico Levin <federico.levin@despegar.com>
2022-09-18 21:41:46 -0300

    yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,16 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 
 console.clear();
 
-const [,,arg3 = 'base=5'] = process.argv;
-const [,base = 5] = arg3.split('=');
+console.log(process.argv);
+console.log(argv);
+console.log(`yargs base: ${argv.base}`)
+
+// const [,,arg3 = 'base=5'] = process.argv;
+// const [,base = 5] = arg3.split('=');
 
 // const base = 3;
-crearArchivo(base)
-    .then(nombreArchivo => console.log(`${nombreArchivo} creado.`))
-    .catch(err => console.log(err));
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(`${nombreArchivo} creado.`))
+//     .catch(err => console.log(err));
