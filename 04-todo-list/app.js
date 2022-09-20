628be8458f128a25ce4c211e00275a8886d452ec
Federico Levin <federico.levin@despegar.com>
2022-09-20 17:54:55 -0300

    todo list - menu

---
+++ 04-todo-list/app.js
@@ -0,0 +1,13 @@
+require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+const main = async() => {
+    let opt = '';
+    do {
+        opt = await mostrarMenu();
+        // await pausa();
+    } while (opt !== '0')
+    
+};
+
+main();
