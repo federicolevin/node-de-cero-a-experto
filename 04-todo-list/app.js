3c43002422ba0f50ae81ff5f7bacf9a40e1d1192
Federico Levin <federico.levin@despegar.com>
2022-09-21 14:37:34 -0300

    todo list - inquirer con pausa

--- 04-todo-list/app.js
+++ 04-todo-list/app.js
@@ -1,10 +1,11 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 const main = async() => {
     let opt = '';
     do {
         opt = await inquirerMenu();
+        if (opt !== '0') await pausa();
     } while (opt !== '0')
     
 };
