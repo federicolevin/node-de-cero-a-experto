88fd060d62a0ba461f0da8b3e4b8faf26a05723e
Federico Levin <federico.levin@despegar.com>
2022-09-28 20:32:54 -0300

    clima-app - modelo para controlar la app

--- 05-clima-app/app.js
+++ 05-clima-app/app.js
@@ -1,8 +1,41 @@
-const { leerInput } = require("./hekpers/inquirer");
+const { inquirerMenu, pausa, leerInput } = require("./hekpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async() => {
-    const texto = await leerInput('Ingresar: ');
-    console.log(texto);
+    const busquedas = new Busquedas();
+    let opt;
+
+    do {
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                // Mostrar mensaje
+                const ciudad = await leerInput('Ciudad: ');
+                console.log(ciudad);
+
+                // Buscar los lugares
+                
+                // Seleccionar el lugar
+                
+                // Clima
+                
+                // Mostrar resultados
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad: ', );
+                console.log('Lat: ', );
+                console.log('Lng: ', );
+                console.log('Temperatura: ', );
+                console.log('Mínima: ', );
+                console.log('Máxima: ', );
+                break;
+        
+            default:
+                break;
+        }
+
+        if (opt !== 0) await pausa();
+    } while (opt !== 0);
 };
 
 main();
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,15 @@
+class Busquedas {
+    historial = ['Buenos Aires', 'Madrid', 'Tel Aviv'];
+
+    constructor() {
+        // TODO: Leer DB si existe
+    }
+
+    async buscarCiudad(ciudad = '') {
+        console.log(ciudad);
+
+        return [];
+    }
+}
+
+module.exports = Busquedas;
