6a44b592eb39b5c8f6195789562dad3505adde39
Federico Levin <federico.levin@despegar.com>
2022-09-28 21:13:23 -0300

    clima-app - axios instance

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -7,11 +7,23 @@ class Busquedas {
         // TODO: Leer DB si existe
     }
 
+    get paramsMapBox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiZmVkZWxldmluMyIsImEiOiJjbDhtYWRoNmswMGJhM29uNGxuZTVqd3VpIn0.0kyei-rY8xfYAg66Mcllew',
+            'language': 'es'
+        }
+    }
+
     async buscarCiudad(ciudad = '') {
         try {
-            const response = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madrid.json?access_token=pk.eyJ1IjoiZmVkZWxldmluMyIsImEiOiJjbDhtYWRoNmswMGJhM29uNGxuZTVqd3VpIn0.0kyei-rY8xfYAg66Mcllew&language=es');
+            const axiosInstance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ciudad}.json`,
+                params: this.paramsMapBox
+            });
+            const response = await axiosInstance.get();
             console.log(response.data);
         } catch (error) {
+            console.log(error);
             return [];
         }
     }
