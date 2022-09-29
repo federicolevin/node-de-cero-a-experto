cc6fecc1dc1de0f6694b946e2237687772543e9d
Federico Levin <federico.levin@despegar.com>
2022-09-29 19:17:00 -0300

    webserver - handlebars

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -3,19 +3,25 @@ const path = require('path');
 const app = express();
 const port = 7080;
 
+app.set('view engine', 'hbs');
+
 // Servir contenido estatico
 app.use(express.static('public/templated-roadtrip'));
 
-app.get('/elements', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/templated-roadtrip/elements.html'));
+app.get('/', (request, response) => {
+    response.render('home');
 });
 
-app.get('/generic', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/templated-roadtrip/generic.html'));
+app.get('/elements', (request, response) => {
+    response.sendFile(path.join(__dirname, '/public/templated-roadtrip/elements.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+app.get('/generic', (request, response) => {
+    response.sendFile(path.join(__dirname, '/public/templated-roadtrip/generic.html'));
+});
+
+app.get('*', (request, response) => {
+    response.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
