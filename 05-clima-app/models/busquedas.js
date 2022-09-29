9f6b1c6f24ecb394776537f86a3e177ea696f9a5
Federico Levin <federico.levin@despegar.com>
2022-09-28 23:21:32 -0300

    clima-app - OpenWeather

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -14,6 +14,14 @@ class Busquedas {
         }
     }
 
+    get paramsOpenWeather() {
+        return {
+            'appid': process.env.OPENWEATHER_KEY,
+            'lang': 'es',
+            'units': 'metric'
+        }
+    }
+
     async buscarCiudad(ciudad = '') {
         try {
             const axiosInstance = axios.create({
@@ -28,6 +36,26 @@ class Busquedas {
                 lng: ciudadRespone.center[0],
                 lat: ciudadRespone.center[1]
             }));
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    async buscarClima(lat, lon) {
+        try {
+            const axiosInstance = axios.create({
+                baseURL: 'http://api.openweathermap.org/data/2.5/weather',
+                params: { ...this.paramsOpenWeather, lat, lon}
+            });
+            const response = await axiosInstance.get();
+            const { weather, main } = response.data;
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
         } catch (error) {
             console.log(error);
             return [];
