85d4cf7ede2b71f19fc3e28103f3219e4b092e92
Federico Levin <federico.levin@despegar.com>
2022-09-30 00:25:40 -0300

    webserver - partials en hbs

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
 const app = express();
 const port = 7080;
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Servir contenido estatico
 app.use(express.static('public/templated-roadtrip'));
 
+const params = {
+    nombre: 'Federico Levin',
+    titulo: 'Curso de Node'
+}
+
 app.get('/', (request, response) => {
-    response.render('home', {
-        nombre: 'Federico Levin',
-        titulo: 'Curso de Node'
-    });
+    response.render('home', params);
 });
 
 app.get('/elements', (request, response) => {
-    response.sendFile(path.join(__dirname, '/public/templated-roadtrip/elements.html'));
+    response.render('elements', params)
 });
 
 app.get('/generic', (request, response) => {
-    response.sendFile(path.join(__dirname, '/public/templated-roadtrip/generic.html'));
+    response.render('generic', params)
 });
 
 app.get('*', (request, response) => {
